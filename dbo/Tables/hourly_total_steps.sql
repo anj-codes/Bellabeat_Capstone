2fe6b2491575a4649ab177dc5a1c17cf33b88b66
anj-codes <120025904+anj-codes@users.noreply.github.com>
2023-05-11 17:39:15 +0800

    change COUNT to SUM

--- dbo/Tables/hourly_total_steps.sql
+++ dbo/Tables/hourly_total_steps.sql
@@ -2,7 +2,19 @@
 
 SELECT 
     [Hour],
-    COUNT (StepTotal) AS total_hourly_steps
+    SUM(StepTotal) AS total_hourly_steps
 FROM 
     bellabeat_capstone.dbo.hourly_steps
     GROUP BY [Hour]
+
+-- I want to see the time with highest total steps
+
+SELECT 
+    [Hour],
+    SUM(StepTotal) AS total_hourly_steps
+FROM 
+    bellabeat_capstone.dbo.hourly_steps
+    GROUP BY [Hour]
+    ORDER BY total_hourly_steps DESC
+
+
