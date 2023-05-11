52b75190e226dde246fb13322f63f87dd166c2b2
anj-codes <120025904+anj-codes@users.noreply.github.com>
2023-05-11 20:08:22 +0800

    better sql statement

--- dbo/Tables/calories_sleep_correlation.sql
+++ dbo/Tables/calories_sleep_correlation.sql
@@ -1,6 +1,16 @@
 -- I want to see the correlation between calories and steps
 
 SELECT 
+    Id,
+    AVG(TotalSteps) AS avg_totalSteps,
+    AVG(Calories) AS avg_totalCalories
+FROM
+    bellabeat_capstone.dbo.daily_activity
+    GROUP BY Id
+
+-- tried using JOIN 
+
+SELECT 
     dbo.daily_calories.Calories,
     dbo.daily_steps.StepTotal
 FROM 
